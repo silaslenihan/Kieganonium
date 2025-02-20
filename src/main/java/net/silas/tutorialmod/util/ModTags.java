e9fdbd913929b4e4299850930ee067970df4d6c6
Silas lenihan <silaslenihan@Silass-MacBook-Pro.local>
2025-02-19 21:39:37 -0500

    custom tags

---
+++ src/main/java/net/silas/tutorialmod/util/ModTags.java
@@ -0,0 +1,25 @@
+package net.silas.tutorialmod.util;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+import net.silas.tutorialmod.TutorialMod;
+import net.minecraft.world.item.Item;
+
+public class ModTags {
+    public static class Blocks {
+        private static TagKey<Block> createTag(String name) {
+            return BlockTags.create(ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+        public static final TagKey<Item> TRANSFORMABLE_ITEMS = createTag("transformable_items");
+
+        private static TagKey<Item> createTag(String name) {
+            return ItemTags.create(ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID, name));
+        }
+    }
+}
