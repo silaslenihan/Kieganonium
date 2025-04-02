4a67d69b9492d1ce38294dcb207d42ec7180963c
Silas Lenihan <sjl@lenihan.net>
2025-04-02 10:41:52 -0400

    disabled old items

--- src/main/java/net/silas/kieganonium/item/ModCreativeModeTabs.java
+++ src/main/java/net/silas/kieganonium/item/ModCreativeModeTabs.java
@@ -20,7 +20,7 @@ public class ModCreativeModeTabs {
                             .title(Component.translatable("creativetab.kieganonium.kieganonium_items"))
                             .displayItems((itemDisplayParameters, output) -> {
                                 output.accept(ModItems.KIEGANONIUM.get());
-                                output.accept(ModItems.RAW_KIEGANONIUM.get());
+//                                output.accept(ModItems.RAW_KIEGANONIUM.get());
 //                                output.accept(ModItems.CHISEL.get());
 //                                output.accept(ModItems.KOHLRABI.get());
 //                                output.accept(ModItems.AURORA_ASHES.get());
@@ -38,32 +38,32 @@ public class ModCreativeModeTabs {
                             })
                             .build());
 
-    public static final RegistryObject<CreativeModeTab> KIEGANONIUM_BLOCKS =
-            CREATIVE_MODE_TABS.register("kieganonium_blocks_tab",
-                    () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.KIEGANONIUM_BLOCK.get()))
-                            .withTabsBefore(KIEGANONIUM_ITEMS.getId())
-                            .title(Component.translatable("creativetab.kieganonium.kieganonium_blocks"))
-                            .displayItems((itemDisplayParameters, output) -> {
-                                output.accept(ModBlocks.KIEGANONIUM_BLOCK.get());
-                                output.accept(ModBlocks.RAW_KIEGANONIUM_BLOCK.get());
-                                output.accept(ModBlocks.MAGIC_BLOCK.get());
-                                output.accept(ModBlocks.KIEGANONIUM_DEEPSLATE_ORE.get());
-                                output.accept(ModBlocks.KIEGANONIUM_ORE.get());
-                                output.accept(ModBlocks.KIEGANONIUM_STAIRS.get());
-                                output.accept(ModBlocks.KIEGANONIUM_SLAB.get());
-
-                                output.accept(ModBlocks.KIEGANONIUM_PRESSURE_PLATE.get());
-                                output.accept(ModBlocks.KIEGANONIUM_BUTTON.get());
-
-                                output.accept(ModBlocks.KIEGANONIUM_FENCE.get());
-                                output.accept(ModBlocks.KIEGANONIUM_FENCE_GATE.get());
-                                output.accept(ModBlocks.KIEGANONIUM_WALL.get());
-
-                                output.accept(ModBlocks.KIEGANONIUM_DOOR.get());
-                                output.accept(ModBlocks.KIEGANONIUM_TRAPDOOR.get());
-                                output.accept(ModBlocks.KIEGANONIUM_LAMP.get());
-                            })
-                            .build());
+//    public static final RegistryObject<CreativeModeTab> KIEGANONIUM_BLOCKS =
+//            CREATIVE_MODE_TABS.register("kieganonium_blocks_tab",
+//                    () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.KIEGANONIUM_BLOCK.get()))
+//                            .withTabsBefore(KIEGANONIUM_ITEMS.getId())
+//                            .title(Component.translatable("creativetab.kieganonium.kieganonium_blocks"))
+//                            .displayItems((itemDisplayParameters, output) -> {
+//                                output.accept(ModBlocks.KIEGANONIUM_BLOCK.get());
+//                                output.accept(ModBlocks.RAW_KIEGANONIUM_BLOCK.get());
+//                                output.accept(ModBlocks.MAGIC_BLOCK.get());
+//                                output.accept(ModBlocks.KIEGANONIUM_DEEPSLATE_ORE.get());
+//                                output.accept(ModBlocks.KIEGANONIUM_ORE.get());
+//                                output.accept(ModBlocks.KIEGANONIUM_STAIRS.get());
+//                                output.accept(ModBlocks.KIEGANONIUM_SLAB.get());
+//
+//                                output.accept(ModBlocks.KIEGANONIUM_PRESSURE_PLATE.get());
+//                                output.accept(ModBlocks.KIEGANONIUM_BUTTON.get());
+//
+//                                output.accept(ModBlocks.KIEGANONIUM_FENCE.get());
+//                                output.accept(ModBlocks.KIEGANONIUM_FENCE_GATE.get());
+//                                output.accept(ModBlocks.KIEGANONIUM_WALL.get());
+//
+//                                output.accept(ModBlocks.KIEGANONIUM_DOOR.get());
+//                                output.accept(ModBlocks.KIEGANONIUM_TRAPDOOR.get());
+//                                output.accept(ModBlocks.KIEGANONIUM_LAMP.get());
+//                            })
+//                            .build());
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
