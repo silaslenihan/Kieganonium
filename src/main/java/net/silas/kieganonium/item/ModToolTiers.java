5eda04773e054290654ce5ba2f37a2898f836254
Silas Lenihan <sjl@lenihan.net>
2025-04-02 10:37:30 -0400

    Buffed Kieganonium tools

--- src/main/java/net/silas/kieganonium/item/ModToolTiers.java
+++ src/main/java/net/silas/kieganonium/item/ModToolTiers.java
@@ -6,7 +6,7 @@ import net.minecraftforge.common.ForgeTier;
 import net.silas.kieganonium.util.ModTags;
 
 public class ModToolTiers {
-    public static final Tier KIEGANONIUM = new ForgeTier(1400, 4, 3f, 20,
+    public static final Tier KIEGANONIUM = new ForgeTier(1400, 50, 3f, 20,
             ModTags.Blocks.NEEDS_KIEGANONIUM_TOOL, () -> Ingredient.of(ModItems.KIEGANONIUM.get()),
             ModTags.Blocks.INCORRECT_FOR_KIEGANONIUM_TOOL);
 
